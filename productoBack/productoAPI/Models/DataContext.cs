ace8d9878537026d0e28889a22171f73e6ef6487
DIego Gutierrez <dmauricio.gs@gmail.com>
2019-03-23 13:38:47 -0400

    api con azure

---
+++ productoBack/productoAPI/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace productoAPI.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<productoAPI.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBack/productoAPI/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace productoAPI.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
